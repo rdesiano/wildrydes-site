94c11e98db57cf34365479ab38b316e05be228de
Richard Desiano <richard.desiano@blackboard.com>
2020-07-20 16:05:16 -0400

    - add cognito user pool and client application configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_7wbKG5EVN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4g1qkgr1jaaj7as8evljp71njj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
